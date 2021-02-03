d96badc78e8a48e28dcf80070511d71c9a4a6e0d
dilution92 <qkswkd99@naver.com>
2021-02-03 15:09:51 +0900

    프로젝트 생성

---
+++ DoubleU/src/main/java/com/doubleU/DoubleUApplication.java
@@ -0,0 +1,13 @@
+package com.doubleU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoubleUApplication.class, args);
+	}
+
+}
