855098804493f72f239f197a735f90350198dc0a
syg0629 <syg0629@hanmail.net>
2021-02-15 13:58:24 +0900

    [게시판] 수정 중..

---
+++ doubleu/src/main/java/com/doubleu/notice/vo/noticeListVo.java
@@ -0,0 +1,61 @@
+package com.doubleu.notice.vo;
+
+public class noticeListVo {
+	int notice_no;
+	String notice_subject;
+	String notice_mid;
+	String notice_date;
+	String notice_type;
+	int notice_hit;
+	String notice_doc;
+	String notice_file;
+	
+	public int getNotice_no() {
+		return notice_no;
+	}
+	public void setNotice_no(int notice_no) {
+		this.notice_no = notice_no;
+	}
+	public String getNotice_subject() {
+		return notice_subject;
+	}
+	public void setNotice_subject(String notice_subject) {
+		this.notice_subject = notice_subject;
+	}
+	public String getNotice_mid() {
+		return notice_mid;
+	}
+	public void setNotice_mid(String notice_mid) {
+		this.notice_mid = notice_mid;
+	}
+	public String getNotice_date() {
+		return notice_date;
+	}
+	public void setNotice_date(String notice_date) {
+		this.notice_date = notice_date;
+	}
+	public String getNotice_type() {
+		return notice_type;
+	}
+	public void setNotice_type(String notice_type) {
+		this.notice_type = notice_type;
+	}
+	public int getNotice_hit() {
+		return notice_hit;
+	}
+	public void setNotice_hit(int notice_hit) {
+		this.notice_hit = notice_hit;
+	}
+	public String getNotice_doc() {
+		return notice_doc;
+	}
+	public void setNotice_doc(String notice_doc) {
+		this.notice_doc = notice_doc;
+	}
+	public String getNotice_file() {
+		return notice_file;
+	}
+	public void setNotice_file(String notice_file) {
+		this.notice_file = notice_file;
+	}
+}
