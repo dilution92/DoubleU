9d9ee01de7cf830f84e83175f93dde793c083c5f
DESKTOP-KIEGI5O\Jeon Yelin <tmqkq97@naver.com>
2021-02-19 03:18:58 +0900

    [주소록] ~02.18 작업

---
+++ doubleu/src/main/java/com/doubleu/member/controller/MemberController.java
@@ -0,0 +1,5 @@
+package com.doubleu.member.controller;
+
+public class MemberController {
+
+}
---
+++ doubleu/src/main/java/com/doubleu/member/vo/MemberVo.java
@@ -0,0 +1,69 @@
+package com.doubleu.member.vo;
+
+public class MemberVo {
+	int member_no;
+	String member_mid;
+	String member_name;
+	String member_email;
+	String member_department;
+	String member_team;
+	int member_phoneNumber;
+	String member_roadAddress;
+	String member_jibunAddress;
+	public int getMember_no() {
+		return member_no;
+	}
+	public void setMember_no(int member_no) {
+		this.member_no = member_no;
+	}
+	public String getMember_mid() {
+		return member_mid;
+	}
+	public void setMember_mid(String member_mid) {
+		this.member_mid = member_mid;
+	}
+	public String getMember_name() {
+		return member_name;
+	}
+	public void setMember_name(String member_name) {
+		this.member_name = member_name;
+	}
+	public String getMember_email() {
+		return member_email;
+	}
+	public void setMember_email(String member_email) {
+		this.member_email = member_email;
+	}
+	public String getMember_department() {
+		return member_department;
+	}
+	public void setMember_department(String member_department) {
+		this.member_department = member_department;
+	}
+	public String getMember_team() {
+		return member_team;
+	}
+	public void setMember_team(String member_team) {
+		this.member_team = member_team;
+	}
+	public int getMember_phoneNumber() {
+		return member_phoneNumber;
+	}
+	public void setMember_phoneNumber(int member_phoneNumber) {
+		this.member_phoneNumber = member_phoneNumber;
+	}
+	public String getMember_roadAddress() {
+		return member_roadAddress;
+	}
+	public void setMember_roadAddress(String member_roadAddress) {
+		this.member_roadAddress = member_roadAddress;
+	}
+	public String getMember_jibunAddress() {
+		return member_jibunAddress;
+	}
+	public void setMember_jibunAddress(String member_jibunAddress) {
+		this.member_jibunAddress = member_jibunAddress;
+	}
+	
+	
+}
