89e547d86b809bd5979de558da08eaadaff395b5
withColinSong <withcolinsong@gmail.com>
2021-02-21 02:32:50 +0900

    [로그인] merge

--- doubleu/src/main/java/com/doubleu/login/controller/loginURLController.java
+++ doubleu/src/main/java/com/doubleu/login/controller/loginURLController.java
@@ -1,9 +1,4 @@
 package com.doubleu.login.controller;
-<<<<<<< HEAD
-=======
-
->>>>>>> 8d6d66a0e872b20df1e257793f020a0239789c76
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
