dcee16c79149e70d6ef6a4d5bcac8c2d266a2c35
DESKTOP-KIEGI5O\Jeon Yelin <tmqkq97@naver.com>
2021-02-28 14:02:00 +0900

    [인사관리] memberVO 가져가세여

--- doubleu/src/main/java/com/doubleu/profile/mybatis/ProfileMapper.java
+++ doubleu/src/main/java/com/doubleu/profile/mybatis/ProfileMapper.java
@@ -5,11 +5,13 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 import com.doubleu.profile.vo.ProfileVo;
+import com.doubleu.profile.vo.WorkVo;
 
 
 @Repository
 @Mapper
 public interface ProfileMapper {
-	public List<ProfileVo> select();
+	public List<ProfileVo> selectProfile();
+	public List<WorkVo> selectWork();
 }
 
