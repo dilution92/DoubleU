dcb7ee059d13d4fc66e6e4f83886f53fb2347b3e
withColinSong <withcolinsong@gmail.com>
2021-03-03 02:54:24 +0900

    [로그인] head--------수정

--- doubleu/src/main/java/com/doubleu/login/controller/loginMainController.java
+++ doubleu/src/main/java/com/doubleu/login/controller/loginMainController.java
@@ -32,179 +32,134 @@ import com.doubleu.notice.vo.NoticeVo;
 @Controller
 public class loginMainController {
 
-	@Autowired
-	LoginService LoginService;
-
-	@Autowired
-	LoginDao daoService;
-
-	@Autowired
-	SelectReceiverService selectApprovalReceiver;
-
-	@Autowired
-	SelectOutgoingService selectApprovalOutgoing;
-
-	//market
-	@Autowired
-	MarketDao marketDao;
-
-	//이메일
-	@Autowired
-	EmailDao DaoService;
-
-	@Autowired
-	SelectSerivce selectService;
-
-	@Autowired
-	FamilyeventService familyeventService;
-
-	@Autowired
-	NoticeService noticeService;
-<<<<<<< HEAD
-
-=======
-	
-	@Autowired
-	CalenderService calender;
-		
->>>>>>> c9e86aef2e144a30024949f91a76f05d5a4be96b
-	// 로그인 체크
-	@RequestMapping(value="/loginCheck", method= {RequestMethod.GET, RequestMethod.POST})
-	public ModelAndView LoginResult(
-			LoginVo loginVo,
-			HttpServletRequest req,
-			HttpSession session,
-			EmailPage page
-			) {
-		session.setMaxInactiveInterval(-1);
-		ModelAndView mv = new ModelAndView();
-		String profileImg;
-
-		System.out.println("컨트롤 :  " + loginVo);
-		loginVo = LoginService.loginCheck(loginVo, session);
-		System.out.println("컨트롤 후 :  " + loginVo);
-
-		if(loginVo == null) {
-			session.setAttribute("member", null);
-			mv.setViewName("redirect:/loginPost");
-		}else {
-			session.setAttribute("member", loginVo);
-			mv.setViewName("MainPage/index");
-<<<<<<< HEAD
-			if(loginVo.getMemberName().equals("정희석")) {
-				profileImg = "/img/profilem.jpg";
-=======
-			
-
-			if(loginVo.getMemberName().equals("정희석")) {
-				 profileImg = "/img/profilem.jpg";
->>>>>>> c9e86aef2e144a30024949f91a76f05d5a4be96b
-			}
-			else if(loginVo.getMemberName().equals("정해준")) {
-				profileImg = "/img/profilehaejun.png";
-			}
-			else if(loginVo.getMemberName().equals("김재현")){
-<<<<<<< HEAD
-				profileImg = "/img/profilekim.png";
-=======
-				 profileImg = "/img/profilekim.png";
->>>>>>> c9e86aef2e144a30024949f91a76f05d5a4be96b
-			}
-			else {
-				profileImg = "/img/profileg.jpg";
-			}
-<<<<<<< HEAD
-			mv.addObject("profileImg", profileImg);
-			//전자결재 불러오기
-=======
-			
-			//전자결재 불러오기
-			mv.addObject("profileImg", profileImg);
->>>>>>> c9e86aef2e144a30024949f91a76f05d5a4be96b
-			Map<String, Object> receiverMap = selectApprovalReceiver.selectReceiver(req, session);
-			mv.addObject("receiverApprovalList", receiverMap.get("list"));
-			Map<String, Object> outgoingMap = selectApprovalOutgoing.selectOutgoing(req, session);
-			mv.addObject("outgoingApprovalList", outgoingMap.get("list"));
-<<<<<<< HEAD
-
-			//market
-			List<MarketVo> marketlist = marketDao.selectMarketMain();
-			mv.addObject("marketList", marketlist);
-
-			//이메일
-			int emailMailBox = 3; //보낸 메일함
-			page.setEmailMailBox(emailMailBox);
-
-			Map<String, Object> map = DaoService.selectPaging(page, session);
-			mv.addObject("EmailList", map.get("pageList"));		
-
-
-			//게시판
-			List<NoticeVo> contentList1 = noticeService.select();
-			mv.addObject("contentList1", contentList1);
-
-			List<FamilyeventVo> contentList = familyeventService.select();
-			mv.addObject("contentList", contentList);
-		}
-
-
-		return mv;
-=======
-			
-			//market
-			List<MarketVo> marketlist = marketDao.selectMarketMain();
-			mv.addObject("marketList", marketlist);
-			
-			//이메일
-			int emailMailBox = 3; //보낸 메일함
-			page.setEmailMailBox(emailMailBox);
-			
-			Map<String, Object> map = DaoService.selectPaging(page, session);
-			mv.addObject("EmailList", map.get("pageList"));		
-			
-			 
-			//게시판
-		    List<NoticeVo> contentList1 = noticeService.select();
-	        mv.addObject("contentList1", contentList1);
-	  	    
-	        List<FamilyeventVo> contentList = familyeventService.select();
-	        mv.addObject("contentList", contentList);
-	        
-	        // 일정
-	        List<CalenderWeekList> list = new ArrayList<>();
-	        list = calender.setMonthCalender(session);
-			int year = calender.getYear();
-			int month = calender.getMonth();
-			int day = calender.getDay();
-			mv.addObject("currentYear", year);
-			mv.addObject("currentMonth", month);
-			mv.addObject("currentDay", day);
-			mv.addObject("calender",list);
-			
-			
-		}
-		
-		
+   @Autowired
+   LoginService LoginService;
+   
+   @Autowired
+   LoginDao daoService;
+   
+   @Autowired
+   SelectReceiverService selectApprovalReceiver;
+   
+   @Autowired
+   SelectOutgoingService selectApprovalOutgoing;
+   
+   //market
+   @Autowired
+   MarketDao marketDao;
+   
+   //이메일
+   @Autowired
+   EmailDao DaoService;
+   
+   @Autowired
+   SelectSerivce selectService;
+   
+   @Autowired
+   FamilyeventService familyeventService;
+   
+   @Autowired
+   NoticeService noticeService;
+   
+   @Autowired
+   CalenderService calender;
+      
+   // 로그인 체크
+   @RequestMapping(value="/loginCheck", method= {RequestMethod.GET, RequestMethod.POST})
+   public ModelAndView LoginResult(
+         LoginVo loginVo,
+         HttpServletRequest req,
+         HttpSession session,
+         EmailPage page
+         ) {
+      session.setMaxInactiveInterval(-1);
+      ModelAndView mv = new ModelAndView();
+      String profileImg;
+      
+      System.out.println("컨트롤 :  " + loginVo);
+      loginVo = LoginService.loginCheck(loginVo, session);
+      System.out.println("컨트롤 후 :  " + loginVo);
+      
+      if(loginVo == null) {
+         session.setAttribute("member", null);
+         mv.setViewName("redirect:/loginPost");
+      }else {
+         session.setAttribute("member", loginVo);
+         mv.setViewName("MainPage/index");
+         
+         if(loginVo.getMemberName().equals("정희석")) {
+             profileImg = "/img/profilem.jpg";
+         }
+         else if(loginVo.getMemberName().equals("정해준")) {
+            profileImg = "/img/profilehaejun.png";
+         }
+         else if(loginVo.getMemberName().equals("김재현")){
+             profileImg = "/img/profilekim.png";
+         }
+         else {
+            profileImg = "/img/profileg.jpg";
+         }
+         
+         //전자결재 불러오기
+         mv.addObject("profileImg", profileImg);
+         Map<String, Object> receiverMap = selectApprovalReceiver.selectReceiver(req, session);
+         mv.addObject("receiverApprovalList", receiverMap.get("list"));
+         Map<String, Object> outgoingMap = selectApprovalOutgoing.selectOutgoing(req, session);
+         mv.addObject("outgoingApprovalList", outgoingMap.get("list"));
+         
+         //market
+         List<MarketVo> marketlist = marketDao.selectMarketMain();
+         mv.addObject("marketList", marketlist);
+         
+         //이메일
+         int emailMailBox = 3; //보낸 메일함
+         page.setEmailMailBox(emailMailBox);
+         
+         Map<String, Object> map = DaoService.selectPaging(page, session);
+         mv.addObject("EmailList", map.get("pageList"));      
+         
+          
+         //게시판
+          List<NoticeVo> contentList1 = noticeService.select();
+           mv.addObject("contentList1", contentList1);
+            
+           List<FamilyeventVo> contentList = familyeventService.select();
+           mv.addObject("contentList", contentList);
+           
+           // 일정
+           List<CalenderWeekList> list = new ArrayList<>();
+           list = calender.setMonthCalender(session);
+         int year = calender.getYear();
+         int month = calender.getMonth();
+         int day = calender.getDay();
+         mv.addObject("currentYear", year);
+         mv.addObject("currentMonth", month);
+         mv.addObject("currentDay", day);
+         mv.addObject("calender",list);
+         
+         
+      }
+      
+      
         
         
         
         return mv;
->>>>>>> c9e86aef2e144a30024949f91a76f05d5a4be96b
-	}
-
-	// 로그아웃
-	@RequestMapping(value="/loginOut", method= {RequestMethod.GET, RequestMethod.POST})
-	public ModelAndView LoginOut(
-			LoginVo vo,
-			HttpServletRequest req,
-			HttpSession session
-			) {
-
-		ModelAndView mv = new ModelAndView();
-
-		System.out.println("세션 초기화 전 : " + session.getAttribute("member"));
-		session.invalidate(); // 세션 초기화
-		mv.setViewName("redirect:/login");
-		return mv;
-	}
+   }
+   
+   // 로그아웃
+   @RequestMapping(value="/loginOut", method= {RequestMethod.GET, RequestMethod.POST})
+   public ModelAndView LoginOut(
+         LoginVo vo,
+         HttpServletRequest req,
+         HttpSession session
+         ) {
+      
+      ModelAndView mv = new ModelAndView();
+      
+      System.out.println("세션 초기화 전 : " + session.getAttribute("member"));
+      session.invalidate(); // 세션 초기화
+      mv.setViewName("redirect:/login");
+      return mv;
+   }
 }
