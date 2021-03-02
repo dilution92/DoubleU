7b1fe8fd404d2b72ad0c7cf27d42956001011dc7
DESKTOP-KIEGI5O\Jeon Yelin <tmqkq97@naver.com>
2021-03-02 21:25:22 +0900

    [인사관리] 회원수정 주소가 안떠............

--- doubleu/src/main/java/com/doubleu/member/service/MemberService.java
+++ doubleu/src/main/java/com/doubleu/member/service/MemberService.java
@@ -1,20 +1,27 @@
 package com.doubleu.member.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.doubleu.member.mybatis.MemberMapper;
 import com.doubleu.member.vo.MemberVo;
+import com.doubleu.profile.vo.WorkVo;
 
 @Service
 public class MemberService {
 	@Autowired
 	MemberMapper mapper;
 	
-	public String update(MemberVo vo) throws Exception{
-		//updateMember(vo);
-		System.out.println("종료");
-		return "member_list.do";
+	public List<MemberVo> selectOne(int no) {
+		return mapper.selectOne(no);
+	}
+	public void memberUpdate(MemberVo vo){
+		System.out.println("update 시작");
+		mapper.memberUpdate(vo);
+		System.out.println("updateMember 완료");
 	}
 }
+
 
